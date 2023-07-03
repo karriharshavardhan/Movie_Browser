93edf4c58c5e136e615a9da9ee89bc4b48024929
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-07-03 19:53:45 +0530

    added about view component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import AboutView from './components/AboutView';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Home/>
+      <AboutView/>
     </div>
   );
 }
