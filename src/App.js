ca61820de05027eaab02bffb2884937145ad3e54
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-07-03 19:05:11 +0530

    added a navbar function component in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 
+function NavBar(){
+  return(
+    <div>
+      This is my NavBar
+    </div>
+  )
+}
 function App() {
   return (
     <div>
+      <NavBar/>
       <h1>Hello from react 201</h1>
     </div>
   );
