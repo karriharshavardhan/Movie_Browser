9448c89fbaaaf3fc16a5d0c650040c241cb0fd4c
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-07-04 17:43:31 +0530

    added a hero component in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
+
+const Hero = () =>{
+  return(
+    <header className="bg-dark text-white p-5">
+      <h1>This is the hero component</h1>
+    </header>
+  )
+}
+
 const Home = () => {
     return(
       <>
+      <Hero/>
         <h1>Hello world from react 201</h1>
       </>
     )
