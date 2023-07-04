b1dea1bcb0cc971ba3d47ce2939b880f342faa8d
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-07-04 17:34:55 +0530

    added links in navbar using router

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
     return(
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="gotosomewhere">Movie Browser</a>
+          <Link className="navbar-brand" to="/">Movie Browser</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="gotosomewhere">Home</a>
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="gotosomewhere">Link</a>
+                <Link className="nav-link" to="/about">About</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" href="gotosomewhere" tabIndex="-1" aria-disabled="true">Disabled</a>
+                <Link className="nav-link disabled" to="/" tabIndex="-1" aria-disabled="true">Coming soon</Link>
               </li>
             </ul>
             <form className="d-flex">
