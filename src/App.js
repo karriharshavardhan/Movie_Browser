cab2eacf44bf6060f2bdf3e7637b0267a206c0bc
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-07-04 17:28:21 +0530

    added routes to react app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import AboutView from './components/AboutView';
+import { Routes, Route } from 'react-router-dom';
+//in latest version of react-router-dom switch is updated or replaced by routes
 
 function App() {
   return (
     <div>
       <NavBar/>
-      <Home/>
-      <AboutView/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/about" element={<AboutView/>}/>
+      </Routes>
     </div>
   );
 }
