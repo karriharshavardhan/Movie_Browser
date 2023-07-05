9a6f0ba67674fa800728a6734f764c7c5d655477
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-07-05 17:49:14 +0530

    added a search component and created a route for it in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import AboutView from './components/AboutView';
+import SearchView from './components/SearchView'
 import { Routes, Route } from 'react-router-dom';
 //in latest version of react-router-dom switch is updated or replaced by routes
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/about" element={<AboutView/>}/>
+        <Route path="/search" element={<SearchView/>}/>
       </Routes>
     </div>
   );
---
+++ src/components/SearchView.js
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+
+const SearchView = () => {
+  return (
+    <>
+      <Hero text="You are searching for......" />
+    </>
+  );
+};
+
+export default SearchView;
