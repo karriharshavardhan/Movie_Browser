408e0e15f63250ee76a1e1435d2b45378119c20e
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-08-20 13:14:23 +0530

    created api reqs in react

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [searchText, setSearchText] = useState('');
 
+  useEffect(() => {
+    console.log(searchText, "is the search text")
+    fetch('https://api.themoviedb.org/3/search/movie?api_key=1b57483b8b14ba0abe86259dd97b537b&language=en-US&query=${searchText}&page=1&include_adult=false')
+      .then(response => response.json)
+      .then(data => {
+        setSearchResults(data.reults)
+      })
+  }, [searchText])
+
   return (
     <div>
       <NavBar searchText={searchText} setSearchText={setSearchText}/>
