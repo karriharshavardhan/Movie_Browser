5d4b2d4b5cdd44da71b63677ae62228721cd2fe3
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-08-20 13:02:10 +0530

    got the api keys

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,5 +1,8 @@
 import Hero from "./Hero";
 
+// tmdb api key =1b57483b8b14ba0abe86259dd97b537b
+// example link = https://api.themoviedb.org/3/search/movie?api_key=1b57483b8b14ba0abe86259dd97b537b&language=en-US&query=star%20wars&page=1&include_adult=false
+
 const SearchView = ({keyword, searchResults}) => {
 
     const title = `You are searching for ${keyword}`
