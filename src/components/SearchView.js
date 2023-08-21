f395502e2a45a307a15beeb78d304cb2f6779d57
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-08-21 11:23:36 +0530

    added bootstrap for movie card

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -3,16 +3,37 @@ import Hero from "./Hero";
 // tmdb api key =1b57483b8b14ba0abe86259dd97b537b
 // example link = https://api.themoviedb.org/3/search/movie?api_key=1b57483b8b14ba0abe86259dd97b537b&language=en-US&query=star%20wars&page=1&include_adult=false
 
+const MovieCard = ({movie}) => {
+  const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+  return (
+    <div className="col-lg-3 col-md-3 col-2 my-4">
+      <div className="card">
+        <img src={posterUrl} className="card-img-top" alt={movie.original_title}/>
+        <div className="card-body">
+          <h5 className="card-title">{movie.original_title}</h5>
+          <a href="#" className="btn btn-primary">Show Details</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const SearchView = ({keyword, searchResults}) => {
   const title = `You are searching for ${keyword}`
   const resultsHtml = searchResults.map((obj,i)=>{
-    return <div key={i}>{obj.original_title}</div>
+    return <MovieCard movie={obj} key={i}/>
   })
 
   return (
     <>
       <Hero text={title} />
-      {resultsHtml}
+      {resultsHtml &&
+      <div className="container">
+        <div className="row">
+          {resultsHtml}
+        </div>
+      </div>
+      }
     </>
   );
 };
