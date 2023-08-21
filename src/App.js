0631e66f5e62117552793dcf119ae28735a5270e
karriharshavardhan <2020meb1289@iitrpr.ac.in>
2023-08-22 00:56:35 +0530

    test:

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import './App.css';
-import {useState, useEffect} from 'react'
-import NavBar from './components/NavBar';
+import { useState, useEffect } from 'react';
+import Navbar from './components/NavBar';
 import Home from './components/Home';
-import AboutView from './components/AboutView';
+import About from './components/AboutView';
 import SearchView from './components/SearchView';
-import MovieView from './components/MovieView';
-import { Routes, Route } from 'react-router-dom';
-//in latest version of react-router-dom switch is updated or replaced by routes
+import MovieView from './components/MovieView'
+import {Route, Routes } from 'react-router-dom'
 
 function App() {
+  const[searchResults, setSearchResults] = useState([]);
+  const[searchText,setSearchText] = useState('');
 
-  const [searchResults, setSearchResults] = useState([]);
-  const [searchText, setSearchText] = useState('');
-
-  useEffect(() => {
+  useEffect(()=>{
     if(searchText){
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=1b57483b8b14ba0abe86259dd97b537b&language=en-US&query=${searchText}&page=1&include_adult=false`)
-        .then(response => response.json)
-        .then(data => {
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=676161c6c2bbe0e6a5727c8440b793bb&language=en-US&query=${searchText}&page=1&include_adult=false`)
+      .then(response=>response.json())
+      .then(data=>{
           setSearchResults(data.results)
-        })
+      })
+
     }
-  }, [searchText])
+  },[searchText])
+
 
   return (
     <div>
-      <NavBar searchText={searchText} setSearchText={setSearchText}/>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route path="/about" element={<AboutView/>}/>
-        <Route path="/search" element={<SearchView keyword={searchText} searchResults={searchResults}/>}/>
-        <Route path="/movies/:id" element={<MovieView/>}/> 
-      </Routes>
+      <Navbar searchText={searchText} setSearchText={setSearchText}/>
+      
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route path="/search" element={<SearchView keyword={searchText} searchResults={searchResults}/>} />
+          <Route exact path="/movie/:id" element={<MovieView/>}/>
+        </Routes>
+    
     </div>
   );
 }
-
 export default App;
--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -1,48 +1,59 @@
 import Hero from "./Hero";
+import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
 
-const MovieView = () => {
+
+
+const MovieView=()=>{
     const {id} = useParams()
-    console.log(id)
+    const [movieDetails, setMovieDetails]= useState({})
+    const [ isLoading, setISLoading] =useState(true)
 
-    const [movieDetails, setMovieDetails] = useState({})
-    const [isLoading, setIsLoading] = useState(true)
+
     useEffect(()=>{
-        console.log('make an api request', id)
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=1b57483b8b14ba0abe86259dd97b537b&language=en-US`)
-            .then(response=>response.json())
-            .then(data=>{
-                setMovieDetails(data)
-                setIsLoading(false)
-            })
-    }, [id])
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=676161c6c2bbe0e6a5727c8440b793bb&language=en-US`)
+        .then(response=>response.json())
+        .then(data=>{
+            setMovieDetails(data)
+            setISLoading(false)
+        })
+    },[id]
+    )
 
     function renderMovieDetails(){
         if(isLoading){
-            return <Hero text="Loading..."/>
+            return <Hero text="Loading...!"/>
         }
+
         if(movieDetails){
-            const posterPath = `https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
-            const backdropUrl= `https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`
-            return (
-                <>
-                    <Hero text={movieDetails.original_title} backdrop={backdropUrl}/>
-                    <div className="container my-5">
-                        <div className="col-md-3">
-                            <img src={posterPath} alt="..." className="img-fluid shadow rounded"/> {/*left col*/}
-                        </div>
-                        <div className="col-md-9">
-                            <h2>{movieDetails.original_title}</h2>
-                            <p className="lead">{movieDetails.overview}</p> {/*Right col*/}
-                        </div>
+            const posterPath=`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`
+            const backdropUrl=`https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`
+            return(
+            <>
+            <Hero text={movieDetails.original_title} backdrop={backdropUrl}/>
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-md-3">
+                        <img src={posterPath} alt="..." className="img-fluid shadow rounded"/>
                     </div>
-                </>
+                    <div className="col-md-9">
+                        <h2>{movieDetails.original_title}</h2>
+                        <p className="lead">
+                            {movieDetails.overview}
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+            </>
             )
         }
-    }
 
-  return renderMovieDetails()
-};
+    }
+    return(
+        renderMovieDetails()
+    )
+}
 
 export default MovieView;
